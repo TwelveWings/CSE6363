4c223c9f3437997313023633fa731f7f6a8ab3d1
TwelveWings <samuel@twistedonyx.com>
2021-11-02 00:30:19 -0500

    Add files via upload

---
+++ recommender_system/movie.py
@@ -0,0 +1,5 @@
+class Movie():
+  def __init__(self, movie_id, title, genre):
+    self.movie_id = int(movie_id)
+    self.title = title
+    self.genre = genre
---
+++ recommender_system/rating.py
@@ -0,0 +1,6 @@
+class Rating():
+  def __init__(self, user_id, movie_id, rating, timestamp):
+    self.user_id = int(user_id)
+    self.movie_id = int(movie_id)
+    self.rating = float(rating)
+    self.timestamp = timestamp
